e88e5dfc64584ffad73e16da125129a121105e03
GiselleBarbosa <g88barbosa@gmail.com>
2025-04-27 14:29:12 -0300

    build:⚙️ adiciona configuracoes para o Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-}
+  preset: "jest-preset-angular",
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$",
+    },
+  },
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/app/**/*.ts",
+    "!src/app/**/*.module.ts",
+    "!src/app/**/*.spec.ts",
+  ],
+};
